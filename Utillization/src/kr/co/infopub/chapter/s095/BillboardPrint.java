a7dfa933fb0b633e4678f465c99270965b3569f2
s2019s28 <s2019s28@e-mirim.hs.kr>
2021-09-10 23:55:26 +0900

    096. 빌보드 차트 정보를 XML로 저장하고 읽기

---
+++ Utillization/src/kr/co/infopub/chapter/s095/BillboardPrint.java
@@ -0,0 +1,6 @@
+// 095. 빌보드 차트 정보를 JSON으로 저장하고 읽기
+package kr.co.infopub.chapter.s095;
+
+public class BillboardPrint {
+
+}
---
+++ Utillization/src/kr/co/infopub/chapter/s096/BillboardPrint.java
@@ -0,0 +1,9 @@
+// 096. 빌보드 차트 정보를 XML로 저장하고 읽기
+package kr.co.infopub.chapter.s096;
+
+public class BillboardPrint {
+
+	public static void main(String[] args) {
+		
+	}
+}
